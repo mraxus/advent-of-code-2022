51a436ddaa959c2eb334beb8d868a1f7e9a02cb1
Marcus Bergman <marcus@boksnok.se>
2022-12-28 13:53:28 +0100

    Added npm run scripts support for running latest test only/showing input

--- test.js
+++ test.js
@@ -1,1 +1,5 @@
-require('./lib').runTests();
+const args = process.argv.slice(2);
+const withInput = args.includes('-i=') || args.includes('--input=');
+const latest = args.includes('-l=') || args.includes('--latest=');
+
+require('./lib').runTests({ withInput, latest });
